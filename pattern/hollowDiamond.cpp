23333ddb265ed8b52cfc23992dc24b61d7338cc3
abhishekkashyap18 <117872546+abhishekkashyap18@users.noreply.github.com>
2023-09-01 21:04:48 +0530

    Create hollowDiamond.cpp

---
+++ pattern/hollowDiamond.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cout << "Enter the value of n" << endl;
+  cin >> n;
+  int num = n / 2;
+
+  // For full pyramid
+  for (int row = 0; row < num; row++) {
+
+    for (int col = 0; col < (num - row - 1); col++) {
+      cout << " ";
+    }
+    for (int col = 0; col < row + 1; col++) {
+      if(col==0 || col==row){
+        cout << "* ";
+      }
+      else{
+        cout<<"  ";
+      }
+      
+    }
+    cout << endl;
+  }
+
+  //For Inverted full Pyramid
+  // outer Loop
+  for (int row = 0; row < num; row++) {
+    // inner Loop-- for spaces
+    for (int col = 0; col < row; col++) {
+      cout << " ";
+    }
+    // loop for starts
+    for (int col = 0; col < (num - row); col++) {
+      if(col==0 || col==(num-row-1)){
+        cout << "* ";
+      }
+      else{
+        cout<<"  ";
+      }
+      
+    }
+    cout << endl;
+  }
+  return 0;
+}
